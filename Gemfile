bd6365b5c0cc6ffd77dea454d6811bf416b77e6e
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-17 12:30:22 +0900

    Revert "sns認証の実装"
    This reverts commit 7ef22f6651c7dda1edcfe22349df22970eb1621b.

--- Gemfile
+++ Gemfile
@@ -63,10 +63,3 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'paranoia'
-
-gem 'omniauth-facebook'
-gem 'omniauth-google-oauth2'
-gem "omniauth-rails_csrf_protection"
-gem 'omniauth', '~>1.9.1'
-
-gem "pry-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   root to: 'works#index'
-  resources :users
 end
