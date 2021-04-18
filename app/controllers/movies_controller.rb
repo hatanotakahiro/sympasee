afa54be3262838e03f668b60e05044479199e0ab
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-18 18:40:57 +0900

    映像作品投稿機能の実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,7 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :admin?, only: [:new, :create, :edit, :update, :destroy]
   def index
   end
 
@@ -7,5 +10,38 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:movie_title, :movie_text, :long, :release_date, :producer, :character, :movie_image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @movie = Movie.find(params[:id])
+  end
+  
+  def admin?
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
