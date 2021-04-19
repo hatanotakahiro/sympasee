35241f99430ddb9e1005da8a2dea7b016bba109c
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-19 22:46:47 +0900

    デプロイseed修正2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.assets.js_compressor = :uglifier
   config.assets.compile = true
+  config.active_job.queue_adapter = :inline
 end
