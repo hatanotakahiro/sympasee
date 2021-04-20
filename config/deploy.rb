10e58e026d7ec285ed31bf3f470412da6276896b
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-04-20 13:09:16 +0900

    デプロイ修正１０

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'sympasee'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:https://github.com/hatanotakahiro/sympasee.git'
+set :repo_url,  'https://github.com/hatanotakahiro/sympasee.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
