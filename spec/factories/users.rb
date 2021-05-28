094bf77e9bf04cd94ca31086dc799a9e85822da0
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-05-28 18:40:13 +0900

    rspecテスト追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6) + "1a"
+    password {password}
+    password_confirmation {password}
   end
 end
