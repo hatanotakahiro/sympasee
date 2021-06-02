e2722a61f543ecc6af16c214ffd88d7dd98fa7ef
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-06-02 22:36:31 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6) + "1a"
     password {password}
     password_confirmation {password}
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
