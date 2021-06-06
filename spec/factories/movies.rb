bdda19181f73ddfd132e9f47bc7304e3e050e8c0
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-06-06 16:12:01 +0900

    映像作品テスト追記１

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :movie do
-    
+    movie_title {Faker::Lorem.characters(number: 50)}
+    movie_text {Faker::Lorem.characters(number:500)}
+    long {Faker::Lorem.characters(number: 30)}
+    producer {Faker::Lorem.characters(number: 50)}
+    character {Faker::Lorem.characters(number: 200)}
+
+    association :user
 
     after(:build) do |movie|
       movie.movie_image.attach(io: File.open('public/images/no_image.jpeg'), filename: 'no_image.jpeg')
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
+  describe '#create' do
+    before do
+      @movie = FactoryBot.build(:movie)
+    end
 
+    context '登録できる場合' do
+      it '映像作品情報が揃っていれば登録できること' do
+        expect(@movie).to be_valid
+      end
+
+    end
+    context '登録できない場合' do
+      it 'movie＿titleが空では登録できないこと' do
+        @movie.movie_title = ''
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Movie title can't be blank")
+      end
+
+      it 'movie_textが空では登録できないこと' do
+        @movie.movie_text = ''
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Movie text can't be blank")
+      end
+
+      it 'longが空では登録できないこと' do
+        @movie.long = ''
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Long can't be blank")
+      end
+
+      it 'producerが空では登録できないこと' do
+        @movie.producer = ''
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Producer can't be blank")
+      end
+
+      it 'characterが空では登録できないこと' do
+        @movie.character = ''
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Character can't be blank")
+      end
+
+      it 'プロフィール画像がなくても登録できること' do
+        @movie.movie_image = nil
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include("Movie image can't be blank")
+      end  
+      it 'userが紐付いていないと保存できないこと' do
+        @movie.user = nil
+        @movie.valid?
+        expect(@movie.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
